628c34d932245e359a068b049b2338bd12b5629a
Tech2cool <aktarvishwas8520@.com>
2024-07-21 19:43:28 +0530

    pages/ half completed

---
+++ src/pages/Recent/Recent.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import Layout from '../../Layout/Layout';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import useDebounce from '../../hooks/useDebounce';
+import { fetchRecentRelease, searchAnime } from '../../api/v1';
+import ListViewItems from '../../component/ListViewItems/ListViewItems';
+import "./Search.css"
+import Top10List from '../../component/Top10List/Top10List';
+import GenreList from '../../component/GenreList/GenreList';
+import { toast } from 'react-toastify';
+import { Item } from '../../utils/contstant';
+
+interface searchQuery{
+    list: Item[];
+    pages: {
+        page:number;
+        is_current:boolean;
+        page_link:string;
+        name:string;
+    }[];
+    currentPage: number;
+    lastPage: number;
+    hasNextPage: boolean;
+    totalPages: number;
+}
+const Recent = () => {
+    const { page } = useParams();
+
+    const deboucePage = useDebounce(page, 500);
+    const {
+        data: dataSearch,
+        error: errorSearch,
+        isLoading: isLoadingSearch,
+    } = useQuery<searchQuery, Error>({
+        queryKey: ['recent-release', deboucePage],
+        queryFn: () => fetchRecentRelease({ page: page }),
+    });
+
+    if(errorSearch){
+        toast(errorSearch?.message)
+    }
+    return (
+        <Layout>
+            <div className='search-list-container'>
+                <div className="search-list-left">
+                <ListViewItems
+                    title={`Recent Releases`}
+                    list={dataSearch?.list}
+                    pagination={true}
+                    isLoading={isLoadingSearch}
+                    pages={dataSearch?.pages}
+                    currentPage={dataSearch?.currentPage}
+                />
+                </div>
+                <div className="search-list-right">
+                    <GenreList />
+                    <Top10List />
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default Recent;
---
+++ src/pages/TopAiring/TopAiring.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import Layout from '../../Layout/Layout';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import useDebounce from '../../hooks/useDebounce';
+import { fetchRecentRelease, fetchTopAiring, searchAnime } from '../../api/v1';
+import ListViewItems from '../../component/ListViewItems/ListViewItems';
+import "./Search.css"
+import Top10List from '../../component/Top10List/Top10List';
+import GenreList from '../../component/GenreList/GenreList';
+import { toast } from 'react-toastify';
+import { Item } from '../../utils/contstant';
+
+interface searchQuery{
+    list: Item[];
+    pages: {
+        page:number;
+        is_current:boolean;
+        page_link:string;
+        name:string;
+    }[];
+    currentPage: number;
+    lastPage: number;
+    hasNextPage: boolean;
+    totalPages: number;
+}
+const TopAiring = () => {
+    const { page } = useParams();
+
+    const deboucePage = useDebounce(page, 500);
+    const {
+        data: dataSearch,
+        error: errorSearch,
+        isLoading: isLoadingSearch,
+    } = useQuery<searchQuery, Error>({
+        queryKey: ['top-airing', deboucePage],
+        queryFn: () => fetchTopAiring({ page: page }),
+    });
+
+    if(errorSearch){
+        toast(errorSearch?.message)
+    }
+    return (
+        <Layout>
+            <div className='search-list-container'>
+                <div className="search-list-left">
+                <ListViewItems
+                    title={`Top Airing`}
+                    list={dataSearch?.list}
+                    pagination={true}
+                    isLoading={isLoadingSearch}
+                    pages={dataSearch?.pages}
+                    currentPage={dataSearch?.currentPage}
+                />
+                </div>
+                <div className="search-list-right">
+                    <GenreList />
+                    <Top10List />
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default TopAiring;
